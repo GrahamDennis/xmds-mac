f70b3b2db3e5d9844b16d59e64a5208d77cf87d0
Graham Dennis <graham.dennis@gmail.com>
2011-11-23 16:36:56 +1100

    Added a script to create a svn checkout of the latest source for xmds2.

---
+++ BuildScripts/xmds-checkout.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -rf xmds2-checkout
+mkdir xmds2-checkout
+cd xmds2-checkout
+
+svn checkout https://xmds.svn.sourceforge.net/svnroot/xmds/trunk/xpdeint .
+
+# Delete the checked out code to reduce size
+find . -delete -not -path \*\.svn\* -and -type f
+cd ..
