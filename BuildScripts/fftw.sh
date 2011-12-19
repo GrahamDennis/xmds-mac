ff45120be8b8dd1481e43af43a50e7c3a410233d
Graham Dennis <graham.dennis@gmail.com>
2011-12-19 18:03:10 +1100

    AVX support for FFTW-3.3
    clang-3.1 is used as the assembler with gcc-mp-4.5 as the compiler.

--- BuildScripts/fftw.sh
+++ BuildScripts/fftw.sh
@@ -2,5 +2,18 @@
 
 source ../../common.sh;
 
+# This is to use our hacked-together clang-3.1 assembler for avx
+export PATH=`pwd`/../../:${PATH}
+
+# We have included this function here because we can only use avx with the 64-bit build.
+function build {
+    export CC="/opt/local/bin/gcc-mp-4.5"
+    ./configure --prefix=$(PWD)/../../output64 OMPI_CC="${CC}" MPICC="mpicc" CC="${CC}" --disable-static --enable-avx $* && make -j4 && make install
+    make clean
+    export CC="cc -arch i386"
+    ./configure --prefix=$(PWD)/../../output32 CC="${CC}" OMPI_CC="${CC}" MPICC=mpicc --disable-static $* && make -j4 && make install
+}
+
+
 build --enable-sse2 --enable-mpi --disable-fortran --enable-threads
 build --enable-sse2 --enable-mpi --disable-fortran --enable-threads --enable-float
