f2580fc9b63e7af46150d14fcd25f3e93bbc8cda
Graham Dennis <graham.dennis@gmail.com>
2012-01-27 10:08:11 +1100

    Moved mathjax-use-tex-fonts.js to xpdeint userdocs

--- BuildScripts/mathjax-use-tex-fonts.js
+++
@@ -1,9 +0,0 @@
-MathJax.Hub.Config({
-  showProcessingMessages: false,
-  messageStyle: "none",
-  "HTML-CSS": {
-    availableFonts: ["TeX"], 
-  },
-});
-
-MathJax.Ajax.loadComplete("http://www.xmds.org/mathjax-use-tex-fonts.js");
