a10bef9b2b4a8fa6e153114734e6b80a81d3af53
Graham Dennis <graham.dennis@gmail.com>
2014-10-02 08:41:15 +1000

    Fix HDF5 buildscript.

--- BuildScripts/hdf5.sh
+++ BuildScripts/hdf5.sh
@@ -4,7 +4,7 @@
 
 cp -p $(PWD)/../../output32/bin/h5cc $(PWD)/../../output32/bin/h5cc.new
 cp -p $(PWD)/../../output64/bin/h5cc $(PWD)/../../output64/bin/h5cc.new
-sed 's/^prefix=".*"$/prefix="${XMDS_USR}"/' $(PWD)/../../output32/bin/h5cc > $(PWD)/../../output32/bin/h5cc.new
-sed 's/^prefix=".*"$/prefix="${XMDS_USR}"/' $(PWD)/../../output64/bin/h5cc > $(PWD)/../../output64/bin/h5cc.new
+sed 's/^prefix=".*"$/prefix="${XMDS_USR}"/;s/ -mmacosx-version-min=10.5//' $(PWD)/../../output32/bin/h5cc > $(PWD)/../../output32/bin/h5cc.new
+sed 's/^prefix=".*"$/prefix="${XMDS_USR}"/;s/ -mmacosx-version-min=10.5//' $(PWD)/../../output64/bin/h5cc > $(PWD)/../../output64/bin/h5cc.new
 mv $(PWD)/../../output32/bin/h5cc.new $(PWD)/../../output32/bin/h5cc
 mv $(PWD)/../../output64/bin/h5cc.new $(PWD)/../../output64/bin/h5cc
